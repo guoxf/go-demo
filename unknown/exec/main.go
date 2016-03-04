c7067b0ec274f8ed1faf97de17b853a3d565b772
郭雄飞 <guoxiongfei@gametrees.com>
2016-03-04 17:16:53 +0800

    1

--- unknown/exec/main.go
+++ unknown/exec/main.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 func main() {
-	cmd := exec.Command("git", "--version")
+	cmd := exec.Command("d:/robot/record.bat")
 	cmd.Stdin = strings.NewReader("")
 	var out bytes.Buffer
 	cmd.Stdout = &out
@@ -18,4 +19,16 @@ func main() {
 		log.Println(err.Error())
 	}
 	fmt.Printf("in all caps: %q\n", out.String())
+}
+
+func testCmd() {
+	cmd := exec.Command("cmd.exe")
+	cmd.Stdout = os.Stdout
+	input, _ := cmd.StdinPipe()
+	cmd.Start()
+	fmt.Fprintln(input, "set GOOS=linux")
+	fmt.Fprintln(input, "set GOOS")
+	fmt.Fprintln(input, "dir")
+	//fmt.Fprintln(input, "exit")
+	cmd.Wait()
 }
