35f1c796729fb52713aadf36526fd00b555716f3
郭雄飞 <guoxiongfei@gametrees.com>
2016-03-15 18:31:29 +0800

    no message

--- test_kingshard/main.go
+++ test_kingshard/main.go
@@ -8,47 +8,23 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "admin:123456@tcp(0.0.0.0:9696)/kingshard")
+	db, err := sql.Open("mysql", "admin:123456@tcp(127.0.0.1:9696)/kingshard")
 	if err != nil {
-		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+		panic(err.Error())
 	}
 	defer db.Close()
-
-	// Prepare statement for inserting data
-	stmtIns, err := db.Prepare("INSERT INTO squareNum VALUES( ?, ? )") // ? = placeholder
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
-
-	// Prepare statement for reading data
-	stmtOut, err := db.Prepare("SELECT squareNumber FROM squarenum WHERE number = ?")
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	defer stmtOut.Close()
+	//	_, err = db.Exec(`begin;insert into test_shard_hash(id,str,f,e,u,i) values(16,"flike",3.14,'test2',2,3);
+	//insert into test_shard_hash(id,str,f,e,u,i) values(17,"flike",3.14,'test2',2,3);commit;`)
+	//	if err != nil {
+	//		panic(err.Error()) // proper error handling instead of panic in your app
+	//	}
 
 	// Insert square numbers for 0-24 in the database
-	for i := 0; i < 25; i++ {
-		_, err = stmtIns.Exec(i, (i * i)) // Insert tuples (i, i^2)
+	for i := 0; i < 2500000; i++ {
+		strSql := fmt.Sprintf(`insert into test_shard_hash(id,str,f,e,u,i) values(%d,"flike",3.14,'test2',2,3);`, i+1)
+		_, err = db.Exec(strSql)
 		if err != nil {
-			panic(err.Error()) // proper error handling instead of panic in your app
+			fmt.Println(err.Error()) // proper error handling instead of panic in your app
 		}
 	}
-
-	var squareNum int // we "scan" the result in here
-
-	// Query the square-number of 13
-	err = stmtOut.QueryRow(13).Scan(&squareNum) // WHERE number = 13
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	fmt.Printf("The square number of 13 is: %d", squareNum)
-
-	// Query another number.. 1 maybe?
-	err = stmtOut.QueryRow(1).Scan(&squareNum) // WHERE number = 1
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	fmt.Printf("The square number of 1 is: %d", squareNum)
 }
