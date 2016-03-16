1aa9293a27ef231a75a3e9bc3d31a93ad092682c
郭雄飞 <guoxiongfei@gametrees.com>
2016-03-16 19:06:29 +0800

    no message

---
+++ test_weedfs/start.bat
@@ -0,0 +1,25 @@
+echo "close weed"
+taskkill /IM weed.exe /f
+
+set rootPath=%~dp0
+echo %rootPath%
+cd /D %rootPath%
+set tmpPath=tmp
+set dataPath=%rootPath%%tmpPath%
+mkdir %tmpPath%
+cd %tmpPath%
+mkdir m1
+mkdir m2
+mkdir m3
+mkdir v1
+mkdir v2
+mkdir v3
+set logPath=%dataPath%\log
+mkdir %logPath%
+start /b weed -v=3 master -port=9333 -mdir=%dataPath%\m1 -peers=localhost:9333,localhost:9334,localhost:9335 -defaultReplication=100 >%logPath%\m1.log 2>&1
+start /b weed -v=3 master -port=9334 -mdir=%dataPath%\m2 -peers=localhost:9333,localhost:9334,localhost:9335 -defaultReplication=100 >%logPath%\m2.log 2>&1
+start /b weed -v=3 master -port=9335 -mdir=%dataPath%\m3 -peers=localhost:9333,localhost:9334,localhost:9335 -defaultReplication=100 >%logPath%\m3.log 2>&1
+
+start /b weed -v=3 volume -port=8081 -dir=%dataPath%\v1 -mserver=localhost:9333 -dataCenter=dc1 >%logPath%\v1.log 2>&1
+start /b weed -v=3 volume -port=8082 -dir=%dataPath%\v2 -mserver=localhost:9334 -dataCenter=dc1 >%logPath%\v2.log 2>&1
+start /b weed -v=3 volume -port=8083 -dir=%dataPath%\v3 -mserver=localhost:9335 -dataCenter=dc2 >%logPath%\v3.log 2>&1
