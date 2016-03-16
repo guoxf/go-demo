8f27c6a1c98fd1c0ed43881bf7dfa9b98bda895d
郭雄飞 <guoxiongfei@gametrees.com>
2016-03-16 12:10:50 +0800

    no message

--- test_kingshard/test_beego_orm.go
+++ test_kingshard/test_beego_orm.go
@@ -14,6 +14,7 @@ func init() {
 	orm.RegisterDriver("mysql", orm.DR_MySQL)
 	orm.RegisterDataBase("default", "mysql", "admin:123456@tcp(127.0.0.1:9696)/kingshard")
 	orm.RegisterModel(new(TestShardJoin))
+	testDel()
 }
 
 func testBeego() {
