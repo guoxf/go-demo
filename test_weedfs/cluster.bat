caf27f067ee593bf9a558136c9df88d00a2ec7f7
郭雄飞 <guoxiongfei@gametrees.com>
2016-03-17 13:58:50 +0800

    no message

--- test_weedfs/cluster.bat
+++ test_weedfs/cluster.bat
@@ -16,6 +16,7 @@ mkdir v2
 mkdir v3
 set logPath=%dataPath%\log
 mkdir %logPath%
+mkdir d:\tmp\f1
 
 set peers=localhost:9333,localhost:9334,localhost:9335
 ::-defaultReplication=100
@@ -26,3 +27,5 @@ start /b weed -v=3 master -port=9335 -mdir=%dataPath%\m3 -peers=%peers% >%logPat
 start /b weed -v=3 volume -port=8081 -dir=%dataPath%\v1 -mserver=localhost:9333 -dataCenter=dc1 >%logPath%\v1.log 2>&1
 start /b weed -v=3 volume -port=8082 -dir=%dataPath%\v2 -mserver=localhost:9334 -dataCenter=dc1 >%logPath%\v2.log 2>&1
 start /b weed -v=3 volume -port=8083 -dir=%dataPath%\v3 -mserver=localhost:9335 -dataCenter=dc2 >%logPath%\v3.log 2>&1
+
+start /b weed filer -port=8888 -dir=d:\tmp\f1 -master=localhost:9333 >d:\tmp\log\f.log 2>&1
--- test_weedfs/main.go
+++ test_weedfs/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"bytes"
-	//	"encoding/json"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/ginuerzh/weedo"
 )
 
 type ResponseData struct {
@@ -19,9 +23,9 @@ type ResponseData struct {
 	Size     int    `json:"size"`
 }
 
-type RequestData struct {
-	FileName string `json:"fileName"`
-	Data     []byte `json:"data"`
+type FilerResponse struct {
+	Name string `json:"name"`
+	Size []byte `json:"size"`
 }
 
 // Creates a new file upload http request with optional extra params
@@ -63,7 +67,41 @@ func newfileUploadRequest(uri string, params map[string]string, paramName, path 
 }
 
 func main() {
-	//fmt.Println(Upload("test.txt"))
+	downFile("http://localhost:8888/image/plate_detect.jpg")
+	downFile("http://localhost:8888/submit/test.txt")
+}
+
+func testSubmit() {
+	request, err := newfileUploadRequest("http://localhost:9333/submit", map[string]string{"aa": "12"}, "upload", "plate_detect.jpg")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		body := &bytes.Buffer{}
+		_, err := body.ReadFrom(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		resp.Body.Close()
+		fmt.Println(resp.StatusCode)
+		fmt.Println(resp.Header)
+		if resp.StatusCode == http.StatusCreated {
+			var rep ResponseData
+			err = json.Unmarshal(body.Bytes(), &rep)
+			fmt.Println(rep, err)
+		} else {
+			fmt.Println(body)
+		}
+
+	}
+}
+
+func testFiler() {
 	request, err := newfileUploadRequest("http://localhost:8888/image/", map[string]string{"aa": "12"}, "file", "plate_detect.jpg")
 	if err != nil {
 		log.Fatal(err)
@@ -82,13 +120,56 @@ func main() {
 		resp.Body.Close()
 		fmt.Println(resp.StatusCode)
 		fmt.Println(resp.Header)
-		//		if resp.StatusCode == http.StatusCreated {
-		//			var rep ResponseData
-		//			err = json.Unmarshal(body.Bytes(), &rep)
-		//			fmt.Println(rep, err)
-		//		} else {
-		fmt.Println(body)
-		//		}
+		if resp.StatusCode == http.StatusCreated {
+			var rep FilerResponse
+			err = json.Unmarshal(body.Bytes(), &rep)
+			fmt.Println(rep, err)
+		} else {
+			fmt.Println(body)
+		}
 
 	}
+}
+
+func testWeedo() {
+	client := weedo.NewClient("localhost:9333", "localhost:8888")
+	dir, err := client.Filer("localhost:8888").Dir("/")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(dir)
+}
+
+func downFile(url string) (pix []byte, err error) {
+	path := strings.Split(url, "/")
+	var name string
+	if len(path) > 1 {
+		name = path[len(path)-1]
+	}
+	fmt.Println(name)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	pix, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(pix))
+	return
+}
+
+func saveFile(b []byte, name string) (int64, error) {
+	out, err := os.Create(name)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	defer out.Close()
+	return io.Copy(out, bytes.NewReader(b))
 }
